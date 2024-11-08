a6515c672ebda755f810fbd2cb9c91fcdb8d7fa5
SamuelRBatista <samuel.r.batista@outlook.com>
2024-11-08 09:58:13 -0300

    first commit

---
+++ ProductApi/Data/DBContextClass.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using ProductApi.Model;
+
+namespace ProductApi.Data
+{
+    public class DBContextClass : DbContext
+    {
+        private readonly IConfiguration Configuration;
+
+        public DBContextClass(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Product> Products { get; set; }
+        
+
+    }
+}
